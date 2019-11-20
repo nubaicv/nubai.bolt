f71426a1fff7351fbd95d18570d0116089baf3fe
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2019-11-20 19:40:11 -0100

    Adelantando algo

--- src/Nubai/FrontendController.php
+++ src/Nubai/FrontendController.php
@@ -18,18 +18,223 @@ use Symfony\Component\HttpFoundation\Response as Response;
  * @author ricardo
  */
 class FrontendController extends BoltController {
+    
+    
+    /**
+     * Controller for the "Homepage" route. Usually the front page of the website.
+     *
+     * @param Request $request
+     *
+     * @return TemplateResponse
+     */
+    public function homepage(Request $request) {
+        
+        // Codigo official do controlador homepage
+        // ---------------------------------------------------------------------------------------------
+        $homepage = $this->getOption('theme/homepage') ?: $this->getOption('general/homepage');
+        $listingParameters = $this->getListingParameters($homepage, true);
+        $content = $this->getContent($homepage, $listingParameters);
 
-    public function helloWorld(Request $request) {
+        $template = $this->templateChooser()->homepage($content);
+        $globals = [];
 
-        $context = [
-            'data' => [
-                'title' => 'My title',
-                'text1' => 'O primeiro texto',
-                'text2' => 'O segunto texto',
-            ]
+        if (is_array($content) && count($content) > 0) {
+            $first = current($content);
+            $globals[$first->contenttype['slug']] = $content;
+            $globals['records'] = $content;
+        } elseif (is_object($content)) {
+            $globals['record'] = $content;
+            $globals[$content->contenttype['singular_slug']] = $content;
+            $globals['records'] = [$content->id => $content];
+        }
+        // ---------------------------------------------------------------------------------------------
+        
+        
+        
+        
+        
+        // Meu codigo aqui
+        // ---------------------------------------------------------------------------------------------
+        
+        $logged_in_message = $this->session()->has('customer_name') ? "We are logged in!" : "We are NOT logged in.";
+        
+        $data_to_template = [
+            'template_data' => [
+                'loggedin' => $logged_in_message,
+                'attributes' => $this->session()->all(),
+                'session_id' => $this->session()->getId(),
+            ],
+        ];
+        
+        
+        // ---------------------------------------------------------------------------------------------
+        
+        
+
+        return $this->render($template, $data_to_template, $globals);
+    }
+
+    
+    
+    /**
+     * The listing page controller.
+     *
+     * @param Request $request         The Symfony Request
+     * @param string  $contenttypeslug The content type slug
+     *
+     * @return TemplateResponse
+     */
+    public function listing(Request $request, $contenttypeslug) {
+        
+        // Codigo official do controlador listing
+        // ---------------------------------------------------------------------------------------------
+        $listingParameters = $this->getListingParameters($contenttypeslug);
+        $content = $this->getContent($contenttypeslug, $listingParameters);
+        $contenttype = $this->getContentType($contenttypeslug);
+
+        $template = $this->templateChooser()->listing($contenttype);
+
+        $globals = [
+            'records'        => $content,
+            $contenttypeslug => $content,
+            'contenttype'    => $contenttype['name'],
+        ];
+        // ---------------------------------------------------------------------------------------------
+        
+        
+        
+        
+        // Meu codigo aqui
+        // ---------------------------------------------------------------------------------------------
+        
+        $logged_in_message = $this->session()->has('customer_name') ? "We are logged in!" : "We are NOT logged in.";
+        
+        $data_to_template = [
+            'template_data' => [
+                'loggedin' => $logged_in_message,
+                'attributes' => $this->session()->all(),
+                'session_id' => $this->session()->getId(),
+            ],
+        ];
+        
+        
+        // ---------------------------------------------------------------------------------------------
+
+        return $this->render($template, $data_to_template, $globals);
+    }
+    
+    
+    
+     /**
+     * Controller for a single record page, like '/page/about/' or '/entry/lorum'.
+     *
+     * @param Request $request         The request
+     * @param string  $contenttypeslug The content type slug
+     * @param string  $slug            The content slug
+     *
+     * @return TemplateResponse
+     */
+    public function record(Request $request, $contenttypeslug, $slug = '') {
+        
+        // Codigo official do controlador record
+        // ---------------------------------------------------------------------------------------------
+        $contenttype = $this->getContentType($contenttypeslug);
+
+        if (isset($contenttype['viewless']) && $contenttype['viewless'] === true) {
+            $this->abort(Response::HTTP_NOT_FOUND, "Page $contenttypeslug/$slug not found.");
+
+            return null;
+        }
+
+        if (empty($slug)) {
+            $slug = $request->get('id');
+        }
+
+        $slug = $this->app['slugify']->slugify($slug);
+
+        $content = $this->getContent($contenttype['slug'], ['slug' => $slug, 'returnsingle' => true, 'log_not_found' => !is_numeric($slug)]);
+
+        if (is_numeric($slug) && !$content) {
+            $content = $this->getContent($contenttype['slug'], ['id' => $slug, 'returnsingle' => true]);
+        }
+        
+        if (!$content) {
+            $this->abort(Response::HTTP_NOT_FOUND, "Page $contenttypeslug/$slug not found.");
+
+            return null;
+        }
+
+        $template = $this->templateChooser()->record($content);
+
+        $this->app['editlink'] = $this->generateUrl('editcontent', ['contenttypeslug' => $contenttype['slug'], 'id' => $content->id]);
+        $this->app['edittitle'] = $content->getTitle();
+
+        $globals = [
+            'record'                      => $content,
+            $contenttype['singular_slug'] => $content,
+        ];
+        // ---------------------------------------------------------------------------------------------
+        
+        
+        
+        
+        // Meu codigo aqui
+        // ---------------------------------------------------------------------------------------------
+        
+        $logged_in_message = $this->session()->has('customer_name') ? "We are logged in!" : "We are NOT logged in.";
+        
+        $data_to_template = [
+            'template_data' => [
+                'loggedin' => $logged_in_message,
+                'attributes' => $this->session()->all(),
+                'session_id' => $this->session()->getId(),
+            ],
+        ];
+        
+        
+        // ---------------------------------------------------------------------------------------------
+
+        return $this->render($template, $data_to_template, $globals);
+    }
+    
+    
+    
+
+    
+    
+    
+    public function login(Request $request) {
+        
+        $this->session()->set('customer_name', 'Ricardo Ponce');
+        
+
+        $logged_in_message = $this->session()->has('customer_name') ? "We are logged in!" : "We are NOT logged in.";
+        
+        $data_to_template = [
+            'template_data' => [
+                'loggedin' => $logged_in_message,
+                'attributes' => $this->session()->all(),
+                'session_id' => $this->session()->getId(),
+            ],
         ];
 
-        return $this->render('mytemplate.twig', $context);
+        return $this->render('mytemplate.twig', $data_to_template);
+    }
+    
+    public function logout(Request $request) {
+        
+        $this->session()->invalidate();
+        
+
+        $logged_in_message = $this->session()->has('customer_name') ? "We are logged in!" : "We are NOT logged in.";
+        
+        $data_to_template = [
+            'template_data' => [
+                'loggedin' => $logged_in_message,
+            ],
+        ];
+
+        return $this->render('mytemplate.twig', $data_to_template);
     }
 
 }
